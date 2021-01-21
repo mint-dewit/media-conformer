0d17186c19f30cd8eda186c56f11b70886d5f78f
Balte de Wit <contact@balte.nl>
2021-01-21 22:07:22 +0100

    fix: configure portable build

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,13 @@
 module.exports = {
 	pluginOptions: {
 		electronBuilder: {
-			mainProcessWatch: ['src/background/**.ts']
+			mainProcessWatch: ['src/background/**.ts'],
+			builderOptions: {
+				win: {
+					target: 'portable',
+					icon: './public/icon.png'
+				}
+			}
 		}
 	}
 }
